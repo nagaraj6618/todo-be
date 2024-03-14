6367e66e6fc467b9809457ddead2a3a4d7d765f8
nagaraj6618 <nagaraj516700@gmail.com>
2024-03-14 22:40:57 +0530

    Fix : changed app.js to index.js

--- route/todoListRoute.js
+++ route/todoListRoute.js
@@ -3,4 +3,4 @@ const router = express.Router()
 const {getAllItems, addTodoItem, updateTodoItem, getList, getAList, deleteAListItem} = require('../controller/todoListController')
 router.route('/').get(getAllItems).post(addTodoItem)
 router.route('/:id').patch(getList,updateTodoItem).get(getList,getAList).delete(getList,deleteAListItem)
-module.exports = router
+module.exports = router;
